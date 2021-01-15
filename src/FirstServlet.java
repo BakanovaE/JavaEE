2b8c0c72315ae0c5e9abb3ecde8fdec77d5d317d
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-15 19:06:07 +0300

    Cookies

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -19,18 +19,26 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
 
-        Cart cart = (Cart) session.getAttribute("cart");
+        String user = (String) session.getAttribute("current user");
 
-        String name = request.getParameter("name");
-        int quantity = Integer.parseInt(request.getParameter("quantity"));
+        if (user == null) {
+            
+        } else {
 
-        if (cart == null) {
-            cart = new Cart();
-            cart.setName(name);
-            cart.setQuantity(quantity);
         }
 
-        session.setAttribute("cart", cart);
+        //Cart cart = (Cart) session.getAttribute("cart");
+
+        //String name = request.getParameter("name");
+        //int quantity = Integer.parseInt(request.getParameter("quantity"));
+
+        //if (cart == null) {
+        //    cart = new Cart();
+        //    cart.setName(name);
+        //    cart.setQuantity(quantity);
+        //}
+
+        //session.setAttribute("cart", cart);
 
         //PrintWriter pw = response.getWriter();
 
